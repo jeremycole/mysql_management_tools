fb6745e3b59cee9bae7ca6c7a93f8b44fe2b503a
Jeremy Cole <jeremy@jcole.us>
2012-10-22 11:41:01 -0700

    Add documentation for alter_test.rb

--- examples/alter_test.rb
+++ examples/alter_test.rb
@@ -12,24 +12,34 @@ module MysqlTableManager
       "Alter a table to add a column named 'b'"
     end
 
+    # Does this class modify anything? If so, we won't call 'apply' if
+    # running in dry-run mode.
     def self.modifies?
       true
     end
 
+    # Would 'apply' do something? Check for the existence of column 'b',
+    # and if it is missing, 'apply' should work.
     def applies?(host, table)      
       ! connection_manager.list_columns(host, table).include?("b")
     end
 
+    # Run the ALTER TABLE to modify the table in a 'forward' direction,
+    # adding the column.
     def apply(host, table)
       connection_manager.query(host,
         "ALTER TABLE #{table} ADD COLUMN b INT NOT NULL"
       )
     end
 
+    # Would 'unapply' do something? Check for the existence of column 'b',
+    # and if it is present, 'unapply' should work.
     def unapplies?(host, table)      
       connection_manager.list_columns(host, table).include?("b")
     end
 
+    # Run the ALTER TABLE to modify the table in a 'backward' direction,
+    # removing the column.
     def unapply(host, table)
       connection_manager.query(host,
         "ALTER TABLE #{table} DROP COLUMN b"
